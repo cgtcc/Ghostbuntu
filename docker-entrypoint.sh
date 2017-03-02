b1e91b221a14d8d49f145a518bdebeb08b6ddcc2
codecoaster <carl.gaudreault@outlook.com>
2017-03-02 01:40:56 -0500

    entry point to pm2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,43 +1,12 @@
 #!/bin/bash
-cat <<-EOH
-###################################
-This Docker script is based on https://github.com/docker-library/ghost/
-http://support.ghost.org/supported-node-versions/
-https://github.com/nodejs/LTS
-
-###################################
-###################################
 
 
-Maintainer of this distribution based on Ubuntu : thecodecoaster@gmail.com
+###################################
+Author : thecodecoaster@gmail.com
 GitRepo: https://github.com/codecoaster/Ghostbuntu
 ###################################
-EOH
 
-set -e
- 
-# allow the container to be started with `--user`
-if [[ "$*" == npm*start* ]] && [ "$(id -u)" = '0' ]; then
-	chown -R user "$GHOST_CONTENT"
-	exec gosu user "$BASH_SOURCE" "$@"
-fi
+cd /usr/src/ghost;
 
-if [[ "$*" == npm*start* ]]; then
-	baseDir="$GHOST_SOURCE/content"
-	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
-		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
-		fi
-	done
+pm2 start -x index.js;
 
-	if [ ! -e "$GHOST_CONTENT/config.js" ]; then
-		sed -r '
-			s/127\.0\.0\.1/0.0.0.0/g;
-			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
-		' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
-	fi
-fi
-
-exec "$@"
